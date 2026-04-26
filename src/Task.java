86333089508bf51504f80debf94cc659e4605f4e
jeremycharlesrfd-wq <jeremy.charlesrfd@gmail.com>
2026-04-26 00:08:08 -0500

    Added a button to the task window (button not functional yet)

--- src/Task.java
+++ src/Task.java
@@ -2,15 +2,18 @@ import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.GraphicsGroup;
 import edu.macalester.graphics.GraphicsObject;
 import edu.macalester.graphics.Rectangle;
+import edu.macalester.graphics.ui.Button;
 import edu.macalester.graphics.ui.TextField;
 
 public class Task extends GraphicsGroup {
     private Rectangle taskBox;
     private TextField textField;
+    private Button button;
 
     public Task(){
         taskBox = new Rectangle(300, 300, 80, 65);
         textField = new TextField();
+        button = new Button("Delete");
     }
 
     public Rectangle getTaskBox() {
@@ -19,5 +22,14 @@ public class Task extends GraphicsGroup {
 
     public void setTaskDescription() {
         textField.setText("Task Description");
+    }
+
+    public Button createButton() {
+        button.setPosition(taskBox.getX() + (taskBox.getWidth()/5), taskBox.getY() + (taskBox.getHeight()/5));
+        return button;
+    }
+
+    public void closeButton() {
+
     }
 }
--- src/TaskTester.java
+++ src/TaskTester.java
@@ -1,17 +1,21 @@
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.GraphicsObject;
 import edu.macalester.graphics.Rectangle;
+import edu.macalester.graphics.ui.Button;
 
 public class TaskTester {
     private static final int CANVAS_WIDTH = 600;
     private static final int CANVAS_HEIGHT = 800;
     private CanvasWindow canvas;
     private Task task;
+    private Button button;
 
     public TaskTester() {
         canvas = new CanvasWindow("Task Manager tester", CANVAS_WIDTH, CANVAS_HEIGHT);
         task = new Task();
+        button = new Button("delete");
         canvas.add(task.getTaskBox());
+        canvas.add(task.createButton());
     }
 
     public static void main(String[] args) {
