74cb030041577e3de9679695526900be84e27720
AnhzzNguyen <150124274+AnhzzNguyen@users.noreply.github.com>
2026-04-28 23:05:27 -0500

    the sound part

--- src/SoundBar.java
+++ src/SoundBar.java
@@ -1,53 +1,93 @@
 import edu.macalester.graphics.*;
+import java.awt.Color;
+import javax.sound.sampled.*;
+import edu.macalester.graphics.ui.Button;
+
+import java.io.File;
+import edu.macalester.graphics.events.*;
+import edu.macalester.graphics.ui.Button;
 
 public class SoundBar {
 
     private GraphicsGroup group;
 
-    private Rectangle track;
-    private Ellipse knob;
     private GraphicsText label;
+    private Clip sound1, sound2, sound3, currentSound;
 
-    private double minX;
-    private double maxX;
-
-    public SoundBar(String name, double x, double y, CanvasWindow canvas) {
+    public SoundBar(String name, double x, double y, CanvasWindow canvas) throws Exception {
         group = new GraphicsGroup();
 
         label = new GraphicsText(name, x, y - 10);
 
-        //track
-        track = new Rectangle(x, y, 200, 8);
-        track.setFilled(true);
-       // track.setFillColor("lightgray");
+        // 3 buttons for sounds
+        Button buttonS1 = new Button("Rain");
+        Button buttonS2 = new Button("Nature");
+        Button buttonS3 = new Button("Coffee Shop");
+        buttonS1.setPosition(50, 100);
+        buttonS2.setPosition(200, 100);
+        buttonS3.setPosition(350, 100);
+        canvas.add(buttonS1);
+        canvas.add(buttonS2);
+        canvas.add(buttonS3);
 
-        //knob
-        knob = new Ellipse(x, y - 6, 12, 20);
-        knob.setFilled(true);
-       // knob.setFillColor("blue");
+        buttonS1.onClick(() -> {
+            selectSound(sound1);
+        });
 
-        minX = x;
-        maxX = x + 200;
+        buttonS2.onClick(() -> {
+            selectSound(sound2);
+        });
 
-        group.add(label);
-        group.add(track);
-        group.add(knob);
+        buttonS3.onClick(() -> {
+            selectSound(sound3);
+        });
 
-        canvas.add(group);
+        //3 options for volume
+        Button buttonV1 = new Button("50%");
+        Button buttonV2 = new Button("100%");
+        Button buttonV3 = new Button("150%");
+        buttonV1.setPosition(50, 150);
+        buttonV2.setPosition(200, 150);
+        buttonV3.setPosition(350, 150);
+        canvas.add(buttonV1);
+        canvas.add(buttonV2);
+        canvas.add(buttonV3);
 
-        //drag to higher/lower sound
-        // canvas.onMouseDrag(event -> {
-        //     if (knob.contains(event.getPosition())) {
-        //         moveKnob(event.getPosition().getX());
-        //     }
-        // });
+        buttonV1.onClick(() -> {
+            setVolume(0.5);
+        });
+
+        buttonV2.onClick(() -> {
+            setVolume(1);
+        });
+
+        buttonV3.onClick(() -> {
+            setVolume(1.5);
+        });
+
+        sound1 = loadSound("rain.wav");
+        sound2 = loadSound("nature.wav");
+        sound3 = loadSound("coffeeshop.wav");
+
+        
     }
 
-    private void moveKnob(double mouseX) {
-        double clampedX = Math.max(minX, Math.min(maxX, mouseX));
-        knob.setPosition(clampedX, knob.getY());
+    private Clip loadSound(String path) throws Exception{
+        Clip sound = AudioSystem.getClip();
+        sound.open(AudioSystem.getAudioInputStream(new File(path)));
+        sound.loop(Clip.LOOP_CONTINUOUSLY);
+        sound.stop();
+        return sound;
     }
 
+    private void selectSound(Clip sound) {
+        if (currentSound != null) currentSound.stop();
+        currentSound = sound;
+        if (currentSound != null) currentSound.start();
+    }
+
+    private void setVolume(double volume){}
+    
     public GraphicsGroup getGraphics() {
         return group;
     }
